d2301656a4e4bbb287f919b77390ab5fe753818a
laisamartini <149402979+laisamartini@users.noreply.github.com>
2024-11-26 19:57:06 -0400

    01

--- projetos_web (1)/projetos_web/projetos_web/projetos_web/settings.py
+++ projetos_web (1)/projetos_web/projetos_web/projetos_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!%6l9qt_y6ppd)9o!bke68c&jp-lvc3nkr)pm^^l-jyrkqrsd^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
